82aa9211e5d9069ad919640c8977de926f78818f
Yaryna Hushuley <yaryna.gushuley@gmail.com>
2025-03-05 17:46:32 +0200

    init server

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import { router } from './routes';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+export { app }
